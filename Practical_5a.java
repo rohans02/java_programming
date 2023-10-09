48723d1975814c0293d6f9606b9c3019ae258fb4
rohans02 <114220852+rohans02@users.noreply.github.com>
2023-10-09 21:00:55 +0530

    Update Practical_5a.java

--- Practical_5a.java
+++ Practical_5a.java
@@ -21,35 +21,3 @@ public class Practical_5a {
 }
 
 
-/*
- * Title: Develop a program for implementation of Arrays in Java
- * Assignment No: 05 (B)
- *
- * Enrollment No: 2106181      Date: 18th Aug 2023
- * */
-public class Practical_5b {
-    public static void main(String args[]) {
-        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
-        int sum = 0;
-
-
-        for (int i = 0; i < arr.length; i++)
-            System.out.println("[" + i + "] : " + arr[i]);
-
-
-        for (int element : arr)
-            sum += element;
-
-
-        System.out.println("Sum = " + sum);
-
-
-        String[] names = new String[2];
-        names[0] = "Eren Yeager";
-        names[1] = "Itachi Uchiha";
-
-
-        for (String name : names)
-            System.out.println(name);
-    }
-}
